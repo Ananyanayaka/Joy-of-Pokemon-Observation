19563c733db6b85412d4094cd92dc97bdc66e58d
Ananyanayaka <99317078+Ananyanayaka@users.noreply.github.com>
2025-02-22 00:11:52 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "alias.h"
+using namespace std;
+
+long long count_combinations(int t, const vector<int>& species) {
+    vector<long long> dp(t + 1, 0);
+    dp[0] = 1;
+    for (int limb : species) {
+        for (int i = limb; i <= t; ++i) {
+            dp[i] += dp[i - limb];
+        }
+    }
+    return dp[t];
+}
+
+int main() {
+    int h;
+    cin >> h;
+    for (int i = 0; i < h; ++i) {
+        int t, s;
+        cin >> t >> s;
+        vector<int> species(s);
+        for (int j = 0; j < s; ++j) {
+            cin >> species[j];
+        }
+        cout << count_combinations(t, species) << endl;
+    }
+    return 0;
+}
